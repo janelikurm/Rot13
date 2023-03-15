9c8538d16f09fa49f158d486a562ca6e5c843fe3
janelikurm <janelikurm@gmail.com>
2023-03-15 11:08:57 +0200

    ROT13 working, three test cases created.

---
+++ src/test/java/org/example/ROT13Test.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ROT13Test {
+
+    ROT13 rot13 = new ROT13();
+
+    @Test
+    void rot13_Tere() {
+        assertEquals("Grer", rot13.rot13("Tere"));
+    }
+    @Test
+    void rot13_Grer() {
+        assertEquals("Tere", rot13.rot13("Grer"));
+    }
+    @Test
+    void rot13_empty() {
+        assertEquals("", rot13.rot13(""));
+    }
+}
