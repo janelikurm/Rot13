6c395d9e4b64552c9bd0b95565dfbb6349f18d27
janelikurm <janelikurm@gmail.com>
2023-03-15 11:43:03 +0200

    refactored, extracted to method.

--- src/main/java/ROT13.java
+++ src/main/java/ROT13.java
@@ -2,27 +2,28 @@ import java.util.Scanner;
 
 public class ROT13 {
     public static void main(String[] args) {
-        ROT13 rot13 = new ROT13();
+        new ROT13().encrypter();
+    }
 
+    private void encrypter() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please insert your input which you want to encrypt or decrypt");
         String userInput = scanner.nextLine();
-        userInput = rot13.rot13(userInput);
+        userInput = rot13(userInput);
         System.out.println(userInput);
         System.out.println("Would you like do reverse this action? Press Y for yes");
         if (scanner.nextLine().equalsIgnoreCase("Y")) {
-            userInput = rot13.rot13(userInput);
+            userInput = rot13(userInput);
             System.out.println(userInput);
         }
         System.out.println("Thank you for using our amazing encrypting and decrypting device! :)");
-
     }
 
-    public String rot13(String s) {
+    public String rot13(String userInput) {
         StringBuilder result = new StringBuilder();
         for (
-                int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+                int i = 0; i < userInput.length(); i++) {
+            char c = userInput.charAt(i);
             if (c >= 'a' && c <= 'm') c += 13;
             else if (c >= 'A' && c <= 'M') c += 13;
             else if (c >= 'n' && c <= 'z') c -= 13;
